5b1db1b71e8433b85bf6287f7c17d12df3126163
Marko Koskinen <markokoskinen2037@gmail.com>
2019-07-28 14:23:32 +0300

    use id instead of class

--- src/components/Player.js
+++ src/components/Player.js
@@ -41,7 +41,7 @@ export default class Player extends Component {
 
   render() {
     return (
-      <div className="player">
+      <div id ="player">
         <ReactPlayer
           url={this.props.url}
           playing
