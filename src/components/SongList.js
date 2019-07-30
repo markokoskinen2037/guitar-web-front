67a69a4e3a695c568f0aad724ac01e0833bee1ce
Marko Koskinen <markokoskinen2037@gmail.com>
2019-07-30 21:16:09 +0300

    implemented favourites checkbox and filtering.

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -6,8 +6,15 @@ export default class SongList extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      filter: ""
+      filter: "",
+      showOnlyFavourites: false
     }
+  }
+
+  toggleShowOnlyFavourites(){
+    this.setState({
+      showOnlyFavourites: !this.state.showOnlyFavourites
+    })
   }
 
 
@@ -15,15 +22,23 @@ export default class SongList extends Component {
 
     const filteredList = this.props.playBackList.filter(song => {
       if (song.name.toLowerCase().includes(this.state.filter.toLowerCase())) {
-        return true
-      } else {
-        return false
+        if(this.state.showOnlyFavourites){
+          return this.props.favourites.includes(song.videoId)
+        } else {
+          return true
+        }
+
       }
+      return false
     })
 
     return (
       <div className="songcontainer">
-        <div className="songSearchContainer"><span>Search for a song ({this.props.playBackList.length}) </span><input value={this.state.filter} onChange={(e) => this.setState({ filter: e.target.value })}></input></div>
+        <div className="songcontainer__songSearchContainer">
+          <span>Search for a song ({this.props.playBackList.length}) </span>
+          <input value={this.state.filter} onChange={(e) => this.setState({ filter: e.target.value })}></input>
+          <span>Show only favourites: </span><input onChange={() => this.toggleShowOnlyFavourites()} checked={this.state.showOnlyFavourites} type="checkbox" ></input>
+          </div>
         {filteredList.map(song => {
 
           return (
